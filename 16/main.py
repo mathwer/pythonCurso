561c13cfecdfeadcce2e7a37ad6cda8f14bd2faf
Matheus Werneck <mat-wer@hotmail.com>
2020-12-21 17:36:14 -0300

    Arquivo turtle e gitignore

---
+++ 16/main.py
@@ -0,0 +1,12 @@
+import turtle
+
+tartaruga1 = turtle.Turtle()
+tartaruga1.color('chartreuse4')
+tartaruga1.shape("turtle")
+tartaruga1.forward(100)
+
+minha_tela = turtle.Screen()
+print(minha_tela.canvheight)
+
+minha_tela.exitonclick()
+
