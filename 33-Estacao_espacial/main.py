424b6eae60cba71bfcb73ed59df8be1b06cdfd0b
Matheus Werneck <mat-wer@hotmail.com>
2021-01-16 12:49:55 -0300

    IRSS é procurada e checa se é noite ou madrugada. Falta enviar o e-mail avisando que ela está no ar

--- 33-Estacao_espacial/main.py
+++ 33-Estacao_espacial/main.py
@@ -1,15 +1,61 @@
 import requests
+from datetime import datetime
+import time
+import smtplib
 
-res = requests.get(url='http://api.open-notify.org/iss-now.json')
+LAT_RIO = -22.906847
+LONG_RIO = -43.172897
 
-if res.status_code != 200:
-    raise Exception('Bad response from ISS API')
 
-# Colocando em JSON
-data = res.json()
-# print(data)
+parametros = {
+    'lat': LAT_RIO,
+    'lng': LONG_RIO,
+    'formatted': 0
+}
 
-longitude = data['iss_position']['longitude']
-latitude = data['iss_position']['latitude']
 
-posicao_iss = (longitude, latitude)
+def noite():
+    res = requests.get(
+        url='https://api.sunrise-sunset.org/json', params=parametros)
+    res.raise_for_status()
+
+    data = res.json()
+    # print(data)
+    nascer_sol = int(data['results']['sunrise'].split('T')[1].split(':')[0])
+    print(nascer_sol)
+    por_sol = int(data['results']['sunset'].split('T')[1].split(':')[0])
+
+    hora_agora = datetime.now()
+
+    if hora_agora >= por_sol or hora_agora <= nascer_sol:
+        return True
+
+
+# Vendo se a IRSS está passando pelo Rio
+def irss_perto():
+    res = requests.get(url='http://api.open-notify.org/iss-now.json')
+    res.raise_for_status()
+    if res.status_code != 200:
+        raise Exception('Bad response from ISS API')
+
+    # Colocando em JSON
+    data = res.json()
+    # print(data)
+
+    irss_longitude = float(data['iss_position']['longitude'])
+    irss_latitude = float(data['iss_position']['latitude'])
+
+    posicao_iss = (irss_longitude, irss_latitude)
+
+    if LONG_RIO - 5 <= irss_longitude <= LONG_RIO + 5 and LAT_RIO - 5 <= irss_latitude <= LAT_RIO + 5:
+        return True
+
+
+def enviar_email():
+    pass
+
+
+procurar_irss = True
+while procurar_irss:
+    irss_perto() and noite()
+    time.sleep(60)
